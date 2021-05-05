44bf8412fbaf2da6e433833b2c7223f62e092619
Lucien Najev <luciennajev@hotmail.com>
2021-05-05 21:48:02 +0100

    heroku config

--- makoto_niijima/settings.py
+++ makoto_niijima/settings.py
@@ -90,11 +90,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-# MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
-
 
 LOGIN_URL = "login"
