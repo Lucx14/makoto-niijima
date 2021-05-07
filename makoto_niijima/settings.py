c1faea24bcda2628ff07a7e03c86f8e0318161a8
Lucien Najev <luciennajev@hotmail.com>
2021-05-07 13:26:31 +0100

    Encryption config

--- makoto_niijima/settings.py
+++ makoto_niijima/settings.py
@@ -6,6 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = int(os.environ.get("DEBUG", default=0))
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 INSTALLED_APPS = [
     "base.apps.BaseConfig",
